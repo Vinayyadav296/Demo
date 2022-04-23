5f1533e7d53cc786f3626e0a3ae3c05022c1aff4
Vinay <yadavvinay296@gmail.com>
2022-04-23 11:40:24 +0530

    Add sum code

---
+++ Sum.java
@@ -0,0 +1,13 @@
+public class Sum {
+    public static void main(String[] args) {
+        Sum obj = new Sum();
+        System.out.println(obj.sum(10, 19));
+
+    }
+
+    public int sum(int a, int b) {
+        int c = a + b;
+        return c;
+    }
+
+}
